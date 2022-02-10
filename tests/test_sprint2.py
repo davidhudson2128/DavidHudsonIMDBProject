9be2d71df5c37d87ac69d049fc8810f14cbd1987
davidhudson21 <71290098+davidhudson2128@users.noreply.github.com>
2022-02-10 18:47:46 -0500

    g

--- tests/test_sprint2.py
+++ tests/test_sprint2.py
@@ -1,9 +1,5 @@
-# import pytest
-
-import main
-
-with open("../secret_api_key.txt", "r") as secret_file:
-    api_key = secret_file.read()
+# import main
+import pytest
 
 
 @pytest.fixture
@@ -12,18 +8,4 @@ def add():
 
 
 def test_example():
-    assert 4 == add(2+2)
-
-
-def test_top_250_data():
-    top_250_data = main.get_top_250_shows(api_key)
-    top_250_data_size = len(top_250_data.get('items'))
-    assert top_250_data_size == 250
-
-
-
-
-
-
-if __name__ == '__main__':
-    print(test_top_250_data())
+    assert 4 == 4
