07d48734246728466520e34b793eaafffc620cc3
davidhudson21 <71290098+davidhudson2128@users.noreply.github.com>
2022-02-10 16:20:43 -0500

    Testing Tests. Adding test_top_250_data() to test_sprint2.py

--- tests/test_sprint2.py
+++ tests/test_sprint2.py
@@ -1,6 +1,9 @@
-# import main
+import main
 import pytest
 
+
+with open("secret_api_key.txt", "r") as secret_file:
+    api_key = secret_file.read()
 
 @pytest.fixture
 def add():
@@ -9,3 +12,10 @@ def add():
 
 def test_example():
     assert 4 == 4
+
+
+def test_top_250_data():
+    top_250_data = main.get_top_250_shows(api_key)
+    top_250_data_size = len(top_250_data.get('items'))
+    assert top_250_data_size == 270
+
