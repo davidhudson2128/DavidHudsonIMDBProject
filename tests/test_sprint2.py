b81e8a1e481223edf63a03adabce774329402e42
davidhudson21 <71290098+davidhudson2128@users.noreply.github.com>
2022-02-10 17:26:02 -0500

    Testing Build Secrets

--- tests/test_sprint2.py
+++ tests/test_sprint2.py
@@ -1,8 +1,8 @@
 import pytest
+
 import main
 
-
-with open("secret_api_key.txt", "r") as secret_file:
+with open("../secret_api_key.txt", "r") as secret_file:
     api_key = secret_file.read()
 
 
@@ -18,4 +18,8 @@ def test_example():
 def test_top_250_data():
     top_250_data = main.get_top_250_shows(api_key)
     top_250_data_size = len(top_250_data.get('items'))
-    assert top_250_data_size == 270
+    assert top_250_data_size == 250
+
+
+if __name__ == '__main__':
+    print(test_top_250_data())
